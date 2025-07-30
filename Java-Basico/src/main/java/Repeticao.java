4c00420ef8a027fa938f949d2da243dd9947f5dc
William dos Santos Rodrigues <williamdossantosrodrigues94@gmail.com>
2025-07-30 12:09:34 -0300

    loops

---
+++ Java-Basico/src/main/java/Repeticao.java
@@ -0,0 +1,17 @@
+public class Repeticao {
+    public static void main(String[] args) {
+        // while = enquanto
+        int  i = 0;
+        while(i <10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Fim do loop while");
+
+        // do - while
+        do {
+            System.out.println(i);
+            i++;
+        }while(i <= 10);
+    }
+}
---
+++ Java-Basico/src/main/java/RepeticaoFor.java
@@ -0,0 +1,15 @@
+public class RepeticaoFor {
+    public static void main(String[] args) {
+        // For = para
+        for (int i = 0; i<=10; i++ ){
+            System.out.println(i);
+        }
+
+        System.out.println("----------------------");
+        // outro metodos
+        int i, j;
+        for (i = 0, j = 9; i<10; i++, j--){
+            System.out.println(i +" - "+ j);
+        }
+    }
+}
