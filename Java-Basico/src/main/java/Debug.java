07db042de1f2bcecb809f8f4420a774316e5f45b
William dos Santos Rodrigues <williamdossantosrodrigues94@gmail.com>
2025-08-04 11:33:59 -0300

    Aula basica concluida, começando a java orientado a  objeto

---
+++ Java-Basico/src/main/java/Debug.java
@@ -0,0 +1,13 @@
+public class Debug {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = x + 5;
+
+        while (y < 10) {
+            System.out.println(y);
+            y += 2;
+            // TODO olha aqui
+        }
+    }
+}
+
---
+++ Java-Basico/src/main/java/Debug2.java
@@ -0,0 +1,9 @@
+public class Debug2 {
+    public static void main(String[] args) {
+        /*
+         deprecated = algo que não deve ser mais usado
+         @Deprecated
+         @SuppressWarnings() = aviso no codigo para indicar melhorias
+        */
+    }
+}
